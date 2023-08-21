716f75dc5f4620647e5f3288309c8b19f8c0b672
Melissa Gwyn <melissagwyn@gmail.com>
2023-08-21 13:37:04 -0400

    both sides work separately

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,56 @@
 import './style.css'
+const teamScoreCounter: number[] = [0, 0]
 
-let teamScoreCounter = 0
 const points = document.querySelectorAll('i')
-const teamScore = document.querySelector('h3')
+const teamScores = document.querySelectorAll('h3')
+const inputBoxes = document.querySelectorAll('input')
+const teamNames = document.querySelectorAll('h2')
+teamNames.forEach((name) => name.innerText)
 
 function clickPoint(event: MouseEvent) {
   const pointClicked = event.target
-
+  let teamNumber = 0
   if (pointClicked instanceof HTMLElement) {
-    if (pointClicked.classList.contains('add') && teamScoreCounter < 21) {
-      teamScoreCounter++
+    if (pointClicked.parentElement?.parentElement?.className == 'team1') {
+      teamNumber = 0
+    } else {
+      teamNumber = 1
     }
-    if (pointClicked.classList.contains('subtract') && teamScoreCounter > 0) {
-      teamScoreCounter--
+    if (
+      pointClicked.classList.contains('add') &&
+      teamScoreCounter[teamNumber] < 21
+    ) {
+      teamScoreCounter[teamNumber]++
+    }
+    if (
+      pointClicked.classList.contains('subtract') &&
+      teamScoreCounter[teamNumber] > 0
+    ) {
+      teamScoreCounter[teamNumber]--
     }
   }
-  if (teamScore instanceof HTMLHeadingElement) {
-    teamScore.textContent = `${teamScoreCounter}`
+  if (teamScores[teamNumber] instanceof HTMLHeadingElement) {
+    teamScores[teamNumber].textContent = `${teamScoreCounter[teamNumber]}`
   }
-  if (teamScoreCounter == 21) {
-    window.alert(`Congratulations ${teamName?.innerText} Won!`)
+  if (teamScoreCounter[teamNumber] == 21) {
+    window.alert(`Congratulations ${teamNames[teamNumber].innerText} Won!`)
   }
 }
 points.forEach((point) => point.addEventListener('click', clickPoint))
 
-const inputBox = document.querySelector('input')
-const teamName = document.querySelector('h2')
-
 function updateTeamName(event: Event) {
   const updatedName = event.target
 
-  if (
-    updatedName instanceof HTMLInputElement &&
-    teamName instanceof HTMLHeadingElement
-  ) {
-    const currentInput = updatedName.value
-    teamName.innerText = currentInput
+  if (updatedName instanceof HTMLInputElement) {
+    if (updatedName.parentElement?.parentElement?.className == 'team1') {
+      const currentInput = updatedName.value
+      teamNames[0].innerText = currentInput
+    } else {
+      const currentInput = updatedName.value
+      teamNames[1].innerText = currentInput
+    }
   }
 }
-inputBox?.addEventListener('input', updateTeamName)
+inputBoxes.forEach((inputBox) =>
+  inputBox?.addEventListener('input', updateTeamName)
+)
